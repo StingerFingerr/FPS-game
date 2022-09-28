3747e271b981e12b11c858754af9c8ef6b8f7eb3
Aleksey Martinovich <martinovichmain@gmail.com>
2022-09-28 16:56:46 +0300

    refactoring

---
+++ Assets/Scripts/Services/Input/IInputService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Infrastructure.Services;
+using UnityEngine;
+
+namespace Services.Input
+{
+    public interface IInputService: IService
+    {
+        event Action Fire;
+
+        event Action StartAiming;
+        event Action FinishAiming;
+
+        event Action StartSprinting;
+        event Action FinishSprinting;
+        
+        event Action<Vector2> Move;
+        event Action<Vector2> Look;
+
+        event Action Jump;
+
+        event Action Crouch;
+        event Action Prone;
+    }
+}
---
+++ Assets/Scripts/Weapon/WeaponSwaySettings.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Weapon
+{
+    public class WeaponSwaySettings: ScriptableObject
+    {
+        [Header("Sway")]
+        public float swayIntensityX = .5f;
+        public float swayIntensityY = .5f;
+        public float aimingSwayIntensityModifier = .5f;
+        public bool swayInvertedX = true;
+        public bool swayInvertedY;
+        public float swayClampX = .15f;
+        public float swayClampY = .15f;
+        public float swaySmooth = 10f;
+
+        [Header("Movement Sway")] 
+        public float movementSwayIntensity = 10f;
+        public float movementSwayClampX = 20f;
+        public float movementSwayClampY = 20f;
+        public float movementSwaySmooth = 3f;
+    }
+}
